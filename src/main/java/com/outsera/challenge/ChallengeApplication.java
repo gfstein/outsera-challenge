a423e5dcaa191fcdc87765c98c448f3721c416d2
Guilherme Stein <you@example.com>
2026-06-11 14:03:43 -0300

    initial commit

---
+++ src/main/java/com/outsera/challenge/ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.outsera.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ChallengeApplication.class, args);
+    }
+
+}
